4fb33d9dc4961ad340fa5e646ffc56055362920a
Yajas Sharma <yajassharma@Yajass-MacBook-Air.local>
2026-03-19 21:21:35 +0530

    solution

--- index.tsx
+++ index.tsx
@@ -1,15 +1,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import './index.css';
 import App from './App';
 
-
-=======
-import App from './App';
-
->>>>>>> ef8a07290aec01352aa31111ed0ecd1bc34923d8
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,29 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-<<<<<<< HEAD
 import tailwindcss from '@tailwindcss/vite';
 
-=======
->>>>>>> ef8a07290aec01352aa31111ed0ecd1bc34923d8
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-<<<<<<< HEAD
-      plugins: [
-        react(),
-        tailwindcss(),
-      ],
+  const env = loadEnv(mode, '.', '');
+  return {
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
 
-=======
-      plugins: [react()],
->>>>>>> ef8a07290aec01352aa31111ed0ecd1bc34923d8
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
